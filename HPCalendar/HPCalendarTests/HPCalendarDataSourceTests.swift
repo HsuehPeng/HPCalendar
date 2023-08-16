b54cb044488f23088790de30cf5d28cc062c34ba
Hsueh Peng Tseng <qaz5899971@gmail.com>
2023-08-16 14:10:43 +0800

    Rename the default test name to HPCalendarDataSource. Create customized test plan

--- HPCalendar/HPCalendarTests/HPCalendarDataSourceTests.swift
+++ HPCalendar/HPCalendarTests/HPCalendarDataSourceTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 @testable import HPCalendar
 
-final class HPCalendarTests: XCTestCase {
+final class HPCalendarDataSourceTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
