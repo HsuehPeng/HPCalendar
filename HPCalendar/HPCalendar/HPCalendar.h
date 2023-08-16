67ff04bb4f2eb333aea9f940b2bb6126c814f7d2
Hsueh Peng Tseng <qaz5899971@gmail.com>
2023-08-16 12:59:38 +0800

    Create HPCalendar framework

---
+++ HPCalendar/HPCalendar/HPCalendar.h
@@ -0,0 +1,18 @@
+//
+//  HPCalendar.h
+//  HPCalendar
+//
+//  Created by Hsueh Peng Tseng on 2023/8/16.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HPCalendar.
+FOUNDATION_EXPORT double HPCalendarVersionNumber;
+
+//! Project version string for HPCalendar.
+FOUNDATION_EXPORT const unsigned char HPCalendarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HPCalendar/PublicHeader.h>
+
+
