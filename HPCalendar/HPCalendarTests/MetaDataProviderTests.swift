66dfc41a4df106b344bfb0f06be82e915964c328
Hsueh Peng Tseng <qaz5899971@gmail.com>
2023-08-16 16:40:13 +0800

    MetaDataProviderTests - Tests every month in 2023 to validate the MetaDataProvider methods

--- HPCalendar/HPCalendarTests/MetaDataProviderTests.swift
+++ HPCalendar/HPCalendarTests/MetaDataProviderTests.swift
@@ -35,29 +35,38 @@ final class MetaDataProviderTests: XCTestCase {
 	
 	func test_getFirstDateOfMonth_generateFirstDateOfMonthByGivenDate() {
 		let (sut, _) = makeSut()
-
-		let firstDateOfTestingMonth = firstDateOfMonthStub()
-		let firstDateOfMonth = sut.getFirstDateOfMonth(for: testingDate)
-				
-		XCTAssertEqual(firstDateOfMonth, firstDateOfTestingMonth)
+		
+		(1...12).forEach { month in
+			let index = month - 1
+			let firstDateOfTestingMonth = firstDateOfMonthStubs()[index]
+			let firstDateOfMonth = sut.getFirstDateOfMonth(for: testingDates[index])
+					
+			XCTAssertEqual(firstDateOfMonth, firstDateOfTestingMonth)
+		}
 	}
 	
 	func test_numbersOfDaysInMonth_getNumbersOfDaysInGivenDate() {
 		let (sut, _) = makeSut()
-
-		let daysOfTestingMonth = daysOfMonthStub()
-		let daysOfMonth = sut.numbersOfDaysInMonth(for: testingDate)
-				
-		XCTAssertEqual(daysOfMonth, daysOfTestingMonth)
+		
+		(1...12).forEach { month in
+			let index = month - 1
+			let daysOfTestingMonth = daysOfMonthStubs()[index]
+			let daysOfMonth = sut.numbersOfDaysInMonth(for: testingDates[index])
+						
+			XCTAssertEqual(daysOfMonth, daysOfTestingMonth)
+		}
 	}
 	
 	func test_firstDateOffSet_getFirstDateWeekDayForGivenDate() {
 		let (sut, _) = makeSut()
-
-		let dateOffsetTestingMonth = firstDateOffSet()
-		let dateOffset = sut.firstDateOffSet(for: testingDate)
-				
-		XCTAssertEqual(dateOffset, dateOffsetTestingMonth)
+		
+		(1...12).forEach { month in
+			let index = month - 1
+			let dateOffsetTestingMonth = firstDateOffSetStubs()[index]
+			let dateOffset = sut.firstDateOffSet(for: testingDates[index])
+					
+			XCTAssertEqual(dateOffset, dateOffsetTestingMonth)
+		}
 	}
 	
 	// MARK: - Helpers
@@ -69,18 +78,43 @@ final class MetaDataProviderTests: XCTestCase {
 		return (sut, calendar)
 	}
 	
-	private let testingDate = Date(timeIntervalSince1970: 1692144000)
-		
-	private func firstDateOfMonthStub() -> Date {
-		return Date(timeIntervalSince1970: 1690848000)
+	private let testingDates = [Date(timeIntervalSince1970: 1673827200), // 2023/01/16
+								Date(timeIntervalSince1970: 1676505600), // 2023/02/16
+								Date(timeIntervalSince1970: 1678924800), // 2023/03/16
+								Date(timeIntervalSince1970: 1681603200), // 2023/04/16
+								Date(timeIntervalSince1970: 1684195200), // 2023/05/16
+								Date(timeIntervalSince1970: 1686873600), // 2023/06/16
+								Date(timeIntervalSince1970: 1689465600), // 2023/07/16
+								Date(timeIntervalSince1970: 1692144000), // 2023/08/16
+								Date(timeIntervalSince1970: 1694822400), // 2023/09/16
+								Date(timeIntervalSince1970: 1697414400), // 2023/10/16
+								Date(timeIntervalSince1970: 1700092800), // 2023/11/16
+								Date(timeIntervalSince1970: 1702684800)  // 2023/12/16
+								
+	]
+	
+	private func firstDateOfMonthStubs() -> [Date] {
+		return [Date(timeIntervalSince1970: 1672531200), // 2023/01/01 00:00
+				Date(timeIntervalSince1970: 1675209600), // 2023/02/01 00:00
+				Date(timeIntervalSince1970: 1677628800), // 2023/03/01 00:00
+				Date(timeIntervalSince1970: 1680307200), // 2023/04/01 00:00
+				Date(timeIntervalSince1970: 1682899200), // 2023/05/01 00:00
+				Date(timeIntervalSince1970: 1685577600), // 2023/06/01 00:00
+				Date(timeIntervalSince1970: 1688169600), // 2023/07/01 00:00
+				Date(timeIntervalSince1970: 1690848000), // 2023/08/01 00:00
+				Date(timeIntervalSince1970: 1693526400), // 2023/09/01 00:00
+				Date(timeIntervalSince1970: 1696118400), // 2023/10/01 00:00
+				Date(timeIntervalSince1970: 1698796800), // 2023/11/01 00:00
+				Date(timeIntervalSince1970: 1701388800)  // 2023/12/01 00:00
+		]
 	}
 	
-	private func daysOfMonthStub() -> Int {
-		return 31
+	private func daysOfMonthStubs() -> [Int] {
+		return [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30 ,31]
 	}
 	
-	private func firstDateOffSet() -> Int {
-		return 3
+	private func firstDateOffSetStubs() -> [Int] {
+		return [1, 4, 4, 7, 2, 5, 7, 3, 6, 1, 4, 6]
 	}
 
 }
