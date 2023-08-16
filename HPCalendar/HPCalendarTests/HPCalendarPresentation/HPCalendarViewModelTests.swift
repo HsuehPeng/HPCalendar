498d6b6309b8ca1c5e5abed3d18406a2d6079996
Hsueh Peng Tseng <qaz5899971@gmail.com>
2023-08-17 00:07:36 +0800

    HPCalendarViewModel - Update days when the baseDate is set

--- HPCalendar/HPCalendarTests/HPCalendarPresentation/HPCalendarViewModelTests.swift
+++ HPCalendar/HPCalendarTests/HPCalendarPresentation/HPCalendarViewModelTests.swift
@@ -6,12 +6,23 @@
 //
 
 import XCTest
+@testable import HPCalendar
 
 class HPCalendarViewModel {
-	var baseDate: Date
+	private let dataSource: CalendarDataSource
 	
-	init(baseDate: Date = Date()) {
+	var baseDate: Date {
+		didSet {
+			days = dataSource.generateDaysInMonth(for: baseDate)
+		}
+	}
+	
+	var days: [HPDay]
+	
+	init(dataSource: CalendarDataSource, baseDate: Date = Date()) {
+		self.dataSource = dataSource
 		self.baseDate = baseDate
+		self.days = dataSource.generateDaysInMonth(for: baseDate)
 	}
 }
 
@@ -19,8 +30,38 @@ final class HPCalendarViewModelTests: XCTestCase {
 	
 	func test_init_setBaseDate() {
 		let currentDate = Date()
-		let sut = HPCalendarViewModel(baseDate: currentDate)
+		let (sut, _) = makeSut(baseDate: currentDate)
 		
 		XCTAssertEqual(sut.baseDate, currentDate)
+	}
+	
+	func test_didSetBaseDate_updateDaysWhenSetBaseDate() {
+		let (sut, dataSource) = makeSut()
+		
+		XCTAssertEqual(dataSource.setDaysCount, 1)
+		
+		sut.baseDate = Date()
+		
+		XCTAssertEqual(dataSource.setDaysCount, 2)
+	}
+	
+	// MARK: - Helpers
+	
+	private func makeSut(baseDate: Date = Date()) -> (HPCalendarViewModel, CalendarDataSourceSpy) {
+		var calendar = Calendar(identifier: .gregorian)
+		calendar.timeZone = .gmt
+		let dataSource = CalendarDataSourceSpy(calendar: calendar)
+		let sut = HPCalendarViewModel(dataSource: dataSource, baseDate: baseDate)
+		
+		return (sut, dataSource)
+	}
+	
+	class CalendarDataSourceSpy: CalendarDataSource {
+		var setDaysCount: Int = 0
+		
+		override func generateDaysInMonth(for date: Date) -> [HPDay] {
+			setDaysCount += 1
+			return []
+		}
 	}
 }
