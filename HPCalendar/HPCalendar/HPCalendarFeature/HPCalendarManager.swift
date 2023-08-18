149d45db866743d830fdadc12c404bfb80b9509c
Hsueh Peng Tseng <qaz5899971@gmail.com>
2023-08-18 18:53:41 +0800

    Create HPCalendarManager class to handle any date calculation

---
+++ HPCalendar/HPCalendar/HPCalendarFeature/HPCalendarManager.swift
@@ -0,0 +1,28 @@
+//
+//  HPCalendarManager.swift
+//  HPCalendar
+//
+//  Created by Hsueh Peng Tseng on 2023/8/18.
+//
+
+class HPCalendarManager {
+	private let calendar: Calendar
+	
+	func addTimeUnit(with component: Calendar.Component, to date: Date) -> Date {
+		return calendar.date(byAdding: component, value: 1, to: date) ?? date
+	}
+	
+	func minusTimeUnit(with component: Calendar.Component, to date: Date) -> Date {
+		return calendar.date(byAdding: component, value: -1, to: date) ?? date
+	}
+	
+	func transformToFormattedDate(from date: Date, by formate: String) -> String {
+		let formatter = DateFormatter()
+		formatter.dateFormat = formate
+		return formatter.string(from: date)
+	}
+	
+	init(calendar: Calendar) {
+		self.calendar = calendar
+	}
+}
