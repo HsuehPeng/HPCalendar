e4c445e89f747952a7008a4b9e5f9f085fa9b227
Hsueh Peng Tseng <qaz5899971@gmail.com>
2023-08-21 15:24:29 +0800

    HPSingleSelectionCalendarCell - Add UI Constraint setting

--- HPCalendar/HPCalendar/HPCalendarUI/SingleSelection/HPSingleSelectionCalendarCell.swift
+++ HPCalendar/HPCalendar/HPCalendarUI/SingleSelection/HPSingleSelectionCalendarCell.swift
@@ -12,17 +12,50 @@ class HPSingleSelectionCalendarCell: UICollectionViewCell {
 	
 	static let reuseId = "\(HPSingleSelectionCalendarCell.self)"
 	
-	let dateLabel: UILabel = {
-		let label = UILabel()
-		label.translatesAutoresizingMaskIntoConstraints = false
-		return label
-	}()
-	
 	let selectionView: UIView = {
 		let view = UIView()
 		view.translatesAutoresizingMaskIntoConstraints = false
 		view.backgroundColor = .clear
 		return view
 	}()
+	
+	let dateLabel: UILabel = {
+		let label = UILabel()
+		label.translatesAutoresizingMaskIntoConstraints = false
+		return label
+	}()
+	
+	// MARK: - LifeCycle
+	
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		
+		setupUI()
+	}
+	
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+	// MARK: - UI
+	
+	private func setupUI() {
+		contentView.addSubview(selectionView)
+		contentView.addSubview(dateLabel)
+		
+		NSLayoutConstraint.activate([
+			selectionView.topAnchor.constraint(equalTo: contentView.topAnchor),
+			selectionView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+			selectionView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+			selectionView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+		])
+		
+		NSLayoutConstraint.activate([
+			dateLabel.centerXAnchor.constraint(equalTo: selectionView.centerXAnchor),
+			dateLabel.centerYAnchor.constraint(equalTo: selectionView.centerYAnchor),
+			dateLabel.widthAnchor.constraint(equalTo: selectionView.widthAnchor, multiplier: 0.6),
+			dateLabel.heightAnchor.constraint(equalTo: selectionView.heightAnchor, multiplier: 0.6)
+		])
+	}
 	
 }
